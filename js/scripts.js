40f85a16f953c785c10a94d4f95dc5f970df91fc
Casual1998 <110164622+Casual1998@users.noreply.github.com>
2025-12-08 11:02:29 +0000

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -106,5 +106,12 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   
   animarBebida();
-  
-  
+
+// Botão de copiar
+function copiarTexto() {
+    const texto = document.getElementById("endereco").innerText;
+
+    navigator.clipboard.writeText(texto).then(() => {
+        alert("Endereço copiado!");
+    });
+}
